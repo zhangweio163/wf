e70d79588f761856914dc39def14788a07fca052
zhangweio163 <109879958+zhangweio163@users.noreply.github.com>
2022-09-08 09:48:34 +0800

    Add files via upload

--- client/http/user.go
+++ client/http/user.go
@@ -1,1 +1,22 @@
+package http
 
+import (
+	"Userclient/endport"
+	"Userclient/user"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func RunServer(port int) {
+	r := gin.Default()
+	r.GET("/user", func(c *gin.Context) {
+		var reqUser *user.UserReq
+		userId := c.Query("id")
+		id, _ := strconv.Atoi(userId)
+		reqUser = &user.UserReq{Userid: int32(id)}
+		c.JSON(http.StatusOK, endport.GetUserEndPort(reqUser))
+	})
+	r.Run(fmt.Sprintf(":%d", port))
+}

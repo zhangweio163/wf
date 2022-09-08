bfbf3b8b6c09822ea74636c4d629560b231e9a65
zhangweio163 <109879958+zhangweio163@users.noreply.github.com>
2022-09-08 09:46:48 +0800

    Add files via upload

--- client/main.go
+++ client/main.go
@@ -1,1 +1,15 @@
+package main
 
+import (
+	"Userclient/endport"
+	"Userclient/http"
+	"flag"
+)
+
+var port = flag.Int("port", 8080, "port")
+
+func main() {
+	endport.InitConf()
+	flag.Parse()
+	http.RunServer(*port)
+}
